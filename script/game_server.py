a476ea2b5ab05b67849080961bcf3d842dabec49
Fubuki909 <wangzhiyuan-990888@live.com>
2022-08-09 21:05:58 -0700

    添加了server发送（尚未测试）

--- script/game_server.py
+++ script/game_server.py
@@ -10,9 +10,6 @@ from lib.Map import Pixcel
 import queue
 
 
-
-
-
 class DrawGameServer:
     def __init__(self, port, players):
         self.port = port
@@ -25,7 +22,6 @@ class DrawGameServer:
         self.max_uid = players + 1
         self.uids = []
 
-
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.bind((self.host, port))
         self.server.listen()
@@ -33,23 +29,11 @@ class DrawGameServer:
         self.receive_drawing_queue = queue()
         self.map_send_queue = queue()
 
-        # self.receiveBUffer
-
-
-
-
     def handle_client(self, client):
         while True:
             try:
-                # message = client.recv(1024)
-
-
                 while True:
-                    time.sleep(1)
-                    message = "hi"
-                    client.send(message.encode('utf-8'))
-
-
+                    self.client_recever(client)
             except:
                 print("there is an error with ", client)
                 break
@@ -58,8 +42,6 @@ class DrawGameServer:
         receive_data = ""
         while True:
             try:
-                # message = client.recv(1024)
-                ######
                 while True:
                     data = client.recv(1024).decode()
                     data_stock = data.split(';')
@@ -68,28 +50,14 @@ class DrawGameServer:
                         # print("\n\n\ndata_js",data_js)
                         if fnmatch(str(data_js),'{"UID": *, "draw_record": *, "more": *}'):
 
-                            # print("=================fnmatch\n")
-                            # return
                             data_json = json.loads(data_js)
                             receive_data.append(data_json)
-                            print(data_json['more'])
+                            
+                            # print(data_json['more'])
+
                             if not data_json['more']:
-
-
-
-
                                 self.receive_drawing_queue.put(receive_data)
                                 receive_data = ""
-
-                                # BreakFlag = False
-
-
-
-                #####
-
-
-
-
             except:
                 print("there is an error with ", client)
                 break
@@ -101,6 +69,8 @@ class DrawGameServer:
 
     def negotiateUID(self):
         while True:
+
+            # have enough play, send start game and exit the function 
             if self.current_uid == self.max_uid:
                 time.sleep(3)
                 self.broadcast("GAMESTART".encode('utf-8'))
@@ -109,8 +79,7 @@ class DrawGameServer:
 
             client, address = self.server.accept()
             print("New player from: ", str(address))
-
-            '''{'UID': 1}'''
+            
             new_play_uid = self.current_uid
             self.uids.append(new_play_uid)
             self.current_uid += 1
@@ -120,11 +89,8 @@ class DrawGameServer:
             send_uid_data = '{"PID": ' + str(new_play_uid) +'}'
             client.send(send_uid_data.encode('utf-8'))
 
-
-
             self.clients.append(client)
             self.clients_address.append(address)
-
 
             thread = threading.Thread(target=self.handle_client, args=(client,))
             thread.start()
@@ -132,41 +98,35 @@ class DrawGameServer:
     def inGame(self):
         map = Map.Map(10,60,10)
 
-        drop_queue_red_line = 10000
-
-
+        drop_queue_red_line = 1000              # if run this amount time the queue still not empty will drop queue 
 
         switch = True
         while True:
             # alternately draw and send
             if switch:
                 switch = not switch
-                # empty receive queue
+                # procuess receive queue
                 loop_counter = 0
                 while not self.receive_drawing_queue.empty():
 
                     map.drawJSON(self.receive_drawing_queue.get())
                     if (loop_counter >= drop_queue_red_line):
                         self.receive_drawing_queue = queue()
-                        #need to add forch to judge map
+                        #need to add force to judge map
                         break
                     loop_counter += 1
 
             else:
+                #send map
                 switch = not switch
+                map_data_str = map.readMapInCellJSON()
 
-    
+                map_data_str = map_data_str.encode('utf-8')
+                self.broadcast(map_data_str)
 
     def run(self):
         print('Server is starting ...')
         self.negotiateUID()
-
-
-        
-
-
-
-
 
 if __name__ == "__main__":
     game_server = DrawGameServer(59000,3)
