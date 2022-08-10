5e08874c82bb3c229737ed990ae8ba33859a3fca
gushenyu1998 <71572428+gushenyu1998@users.noreply.github.com>
2022-08-09 19:09:53 -0700

    更新了文字实时刷新功能 画面实时刷新的代码已经完成了，但是还需要debug

--- lib/Draw Panel.py
+++ lib/Draw Panel.py
@@ -24,7 +24,11 @@ UID_list = [0, 1, 2, 3, 4]
 current_picture = np.zeros((600, 600), dtype=int)
 lock_list = np.ones((10, 10), dtype=int)
 
-time_sleep = 3
+text_game = "Your color is: {}\n"
+player1 = "first player is: Player{}, {}"
+player2 = "Second player is: Player{}, {}"
+player3 = "Third player is: Player{}, {}"
+player4 = "Last player is: Player{}, {}"
 
 
 def delete_list_duplicate():
@@ -34,7 +38,7 @@ def delete_list_duplicate():
     draw_data = [dict(t) for t in set([tuple(d.items()) for d in draw_data])]
 
 
-def client_update(cell=None, islock=True):
+def client_update(cell=None):
     if cell is not None:
         x_axis = (cell['index'] % 10) * 60
         y_axis = int(cell['index'] / 10) * 60
@@ -47,18 +51,26 @@ def client_update(cell=None, islock=True):
 
 
 def game_check():
-    Players = []
-    for UID in UID_list:
-        temp = UID_list.count(UID)
-        Players.append(temp / 360000)
-    return Players
+    # Players = []
+    # for UID in UID_list:
+    #     if UID == 0:
+    #         continue
+    #     temp = (np.sum(current_picture == UID)) / 360000
+    #     Players.append({'UID': UID, 'percentage': temp})
+
+    test = [{'UID': 1, 'percentage': 0.1},
+            {'UID': 2, 'percentage': 0.2},
+            {'UID': 3, 'percentage': int(time.time())},
+            {'UID': 4, 'percentage': 0.3}]
+    new = sorted(test, key=lambda k: k.__getitem__('percentage'))
+    return new
 
 
 def check_win():
     p = game_check()
     nth_player = 0
     for temp in p:
-        if temp >= 0.5:
+        if temp['percentage'] >= 0.5:
             return True, nth_player
         nth_player += 1
     return False, 0
@@ -111,7 +123,11 @@ class Brush(object):
                         if abs(x) + abs(y) <= paint_broad:
                             x_axis = p[0] + x
                             y_axis = p[1] + y
-                            if lock_list[int(x_axis / 60)][int(y_axis / 60)]:
+                            if x_axis >= 600 or y_axis>=600:
+                                pass
+
+
+                            if lock_list[int(x_axis / 60)-3][int(y_axis / 60)-3]:
                                 pg.draw.circle(self.screen, self.color, (x_axis, y_axis), 1)
                                 message = {'UID': Client_UID, 'draw_record': (int(x_axis), int(y_axis)), 'more': True}
                                 draw_data.append(message)
@@ -122,7 +138,7 @@ class Brush(object):
 class Painter:
     def __init__(self, Sock):
         pg.display.set_caption("Test Test Test")
-        self.window = (600, 700)
+        self.window = (600, 800)
         self.clock = pg.time.Clock()
         self.screen = pg.display.set_mode(self.window, 0, 32)
         self.brush = Brush(self.screen)
@@ -130,8 +146,10 @@ class Painter:
         self.sock = Sock
 
     def run(self):
+        pg.init()
         self.screen.fill((255, 255, 255))
         self.draw_game_line()
+
         while True:
             self.clock.tick(600)
             for event in pg.event.get():
@@ -141,6 +159,7 @@ class Painter:
                         event.type == MOUSEMOTION or \
                         event.type == MOUSEBUTTONUP:
                     self.paint_judgement(position=event.pos, event_type=event.type)
+            self.text_update()
             if len(draw_data) > 10240:
                 delete_list_duplicate()
                 self.sending_data()
@@ -162,21 +181,22 @@ class Painter:
         self.draw_game_line()
 
     def paint_judgement(self, position, event_type):
-        if event_type == MOUSEBUTTONDOWN:
-            self.brush.start(position)
-            message = {'UID': Client_UID, 'draw_record': position, 'more': True}
-            draw_data.append(message)
-        elif event_type == MOUSEMOTION:
-            self.brush.Draw(position)
-        elif event_type == MOUSEBUTTONUP:
-            self.brush.close()
-            message = {'UID': Client_UID, 'draw_record': position, 'more': False}
-            delete_list_duplicate()
-            for i in range(10):
+        if position[0] <= 600 or position[1] <= 600:
+            if event_type == MOUSEBUTTONDOWN:
+                self.brush.start(position)
+                message = {'UID': Client_UID, 'draw_record': position, 'more': True}
                 draw_data.append(message)
-            print("mouse up triggered: \n", lock_list)
+            elif event_type == MOUSEMOTION:
+                self.brush.Draw(position)
+            elif event_type == MOUSEBUTTONUP:
+                self.brush.close()
+                message = {'UID': Client_UID, 'draw_record': (min(position[0],600),min(position[1],600)), 'more': False}
+                delete_list_duplicate()
+                for i in range(10):
+                    draw_data.append(message)
+                # print("mouse up triggered: \n", lock_list)
+                self.sending_data()
 
-            self.sending_data()
 
     def sending_data(self):
         while len(draw_data) != 0:
@@ -186,16 +206,31 @@ class Painter:
             self.sock.send(string.encode())
         return
 
-    def receive_data(self):
-        data = self.sock.recv(10240).decode()
-        data_stock = data.split(';;')
-        while len(data_stock) != 0:
-            data_js = data_stock.pop()
-            if fnmatch(str(data_js), '{"index": *, "islock": *, "loc": *}'):
-                data_json = json.loads(data_js)
-                client_update(data_json)
-                self.Draw_update(current_picture)
+    def text_update(self):
+        rect = pg.Surface((600, 200))
+        rect.fill((255, 255, 255))
+        self.screen.blit(rect, (0, 600))
+        game_proccess = game_check()
 
+        game_text1 = player1.format(game_proccess[0]['UID'], game_proccess[0]['percentage'])
+        font_t = pg.font.SysFont('arial', 30)
+        text = font_t.render(game_text1,True,(0, 0, 0))
+        self.screen.blit(text, (20,620))
+
+        game_text2 = player2.format(game_proccess[1]['UID'], game_proccess[1]['percentage'])
+        font_t = pg.font.SysFont('arial', 30)
+        text = font_t.render(game_text2, True, (0, 0, 0))
+        self.screen.blit(text, (20, 660))
+
+        game_text3 = player3.format(game_proccess[2]['UID'], game_proccess[2]['percentage'])
+        font_t = pg.font.SysFont('arial', 30)
+        text = font_t.render(game_text3, True, (0, 0, 0))
+        self.screen.blit(text, (20, 700))
+
+        game_text4 = player4.format(game_proccess[3]['UID'], game_proccess[3]['percentage'])
+        font_t = pg.font.SysFont('arial', 30)
+        text = font_t.render(game_text4, True, (0, 0, 0))
+        self.screen.blit(text, (20, 740))
 
 
 class TCP_client:
@@ -210,22 +245,36 @@ class TCP_client:
             print('connection not success')
             return
 
-    def run(self):
+    def get_painter(self):
+        return self.Painter
 
+    def receive_message(self):
+        while True:
+            time.sleep(0.1)
+            data = self.sock.recv(1024).decode()
+            data_stock = data.split(';;')
+            while len(data_stock) != 0:
+                data_js = data_stock.pop()
+                if fnmatch(str(data_js), '{"index": *, "islock": *, "loc": *}'):
+                    data_json = json.loads(data_js)
+                    client_update(data_json)
+                    self.Painter.Draw_update(current_picture)
+                if fnmatch(str(data_js), '{"UID":*, "color":*}'):
+                    data_json = json.loads(data_js)
+                    Client_UID = data_json['UID']
+                    color = data_json['color']
+
+    def run(self):
         global color
         color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
         self.Painter.run()
 
-    def Paint_run(self):
-        pass
 
-    def receiver_run(self):
-        pass
-
-def infinitloop():
+def server_run_proccess():
     server.server()
 
-def paint_run():
+
+def client_run_proccess():
     time.sleep(1)
     app = TCP_client()
     app.run()
@@ -233,34 +282,9 @@ def paint_run():
 
 if __name__ == '__main__':
     q = Queue(2)
-    p2 = Process(target=infinitloop)
-    p1 = Process(target=paint_run)
+    p2 = Process(target=server_run_proccess)
+    p1 = Process(target=client_run_proccess)
     p1.start()
     p2.start()
     p1.join()
     p2.join()
-    # ink = []
-    # map = Map.Map(map_cell, cell_pixel_length, 4)
-    # for i in range(600):
-    #     for j in range(600):
-    #         ran = random.randint(0, 1)
-    #         ink.append(Pixcel.Pixcel(ran, time.time(), False, False, i, j, True))
-    # ink[-1] = Pixcel.Pixcel(0, time.time(), False, False, 599, 599, False)
-    # map.draw(ink)
-    # map_temp = map.readWholeMap()
-    # k = 0
-    #
-    # map_a = np.zeros((600, 600), dtype=int)
-    # for i in range(600):
-    #     for j in range(600):
-    #         map_a[i][j] = map_temp[0][k]['UID']
-    #         k += 1
-    #
-    # color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-    # app = Painter()
-    # app.run()
-
-    # temp = []
-    # with open('../yaml_test.yaml','r') as f:
-    #     temp = yaml.load(f.read())
-    #     print(temp[0])
