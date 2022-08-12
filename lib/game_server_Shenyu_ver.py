a01b5a2f1a606b8533bd4175bcd77894eabfb21d
gushenyu1998 <71572428+gushenyu1998@users.noreply.github.com>
2022-08-11 18:46:49 -0700

    完成主要功能

---
+++ lib/game_server_Shenyu_ver.py
@@ -0,0 +1,209 @@
+import json
+import socket
+import threading
+import time
+import queue
+import traceback
+from fnmatch import fnmatch
+import numpy as np
+
+
+
+class DrawGameServer:
+    def __init__(self, port, players):
+        self.port = port
+        self.host = '127.0.0.1'
+
+        self.clients = []
+        self.clients_address = []
+
+        self.current_uid = 1
+        self.max_uid = players + 1
+        self.uids = []
+
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.bind((self.host, port))
+        self.server.listen()
+
+        self.receive_drawing_queue = queue.Queue()
+        # self.map_send_queue = queue()
+
+        self.map = np.zeros((50, 50), dtype=int)
+        self.lock_list = np.zeros((10, 10), dtype=int)
+
+        self.lock = threading.Lock()
+        self.last_message = ""
+
+    def handle_client(self, client):
+        while True:
+            try:
+                while True:
+                    self.client_recever(client)
+            except Exception as e:
+                # print("there is an error, detail: ", repr(e))
+                break
+
+    def client_recever(self, client):
+        while True:
+            try:
+                while True:
+                    data = client.recv(1024).decode()
+                    data_stock = data.split(';')
+                    if len(data_stock) != 0:
+                        temp = data_stock[0] + self.last_message
+                        data_stock.insert(0, temp)
+                    while len(data_stock) != 0:
+                        data_js = data_stock.pop()
+                        if fnmatch(str(data_js), '{"UID": *, "draw_record": *, "more": *}'):
+                            data_json = json.loads(data_js)
+                            self.lock.acquire()
+                            self.receive_drawing_queue.put(data_json)
+                            self.lock.release()
+            except Exception as e:
+                print("Client handel exception", repr(e))
+                break
+
+    def broadcast(self, message):
+        for client in self.clients:
+            client.send(message)
+
+    def negotiateUID(self):
+        while True:
+
+            # have enough play, send start game and exit the function 
+            if self.current_uid == self.max_uid:
+                # time.sleep(3)
+                # self.broadcast("GAMESTART".encode('utf-8'))
+                print("Negotiate UID finish.")
+                return
+
+            client, address = self.server.accept()
+            print("New player from: ", str(address))
+
+            new_play_uid = self.current_uid
+            self.uids.append(new_play_uid)
+            self.current_uid += 1
+
+            print("Assign uid", new_play_uid)
+
+            send_uid_data = ';;{"PID": ' + str(new_play_uid) + '};;'
+            for i in range(2):
+                client.send(send_uid_data.encode('utf-8'))
+
+            self.clients.append(client)
+            self.clients_address.append(address)
+
+            thread = threading.Thread(target=self.handle_client, args=(client,))
+            thread.start()
+
+    def broadcast_map(self):
+        while True:
+            time.sleep(10)
+            for i in range(50):
+                for j in range(50):
+                    message = {"UID": int(self.map[i][j]), "loc": (i, j)}
+                    sending = json.dumps(message) + ";;"
+                    self.broadcast(sending.encode())
+
+            for i in range(10):
+                for j in range(10):
+                    message = {"islock": int(self.lock_list[i][j]), "loc": (i, j)}
+                    sending = json.dumps(message) + ";;"
+                    self.broadcast(sending.encode())
+
+    def pixel_proccess(self):
+        try:
+            while True:
+                data_json = self.receive_drawing_queue.get()
+                UID = data_json['UID']
+                if data_json['more']:
+                    x = data_json['draw_record'][0]
+                    y = data_json['draw_record'][1]
+                    if x >= 50 or y >= 50 or x < 0 or y < 0:
+                        continue
+                    if self.map[x][y] != 0:
+                        continue
+                    elif self.lock_list[int(x / 5)][int(y / 5)] != 0 and \
+                            self.lock_list[int(x / 5)][int(y / 5)] != UID:
+                        continue
+                    else:
+                        sending_pixel = {"UID": UID, "loc": (x, y)}
+                        message_pixel = json.dumps(sending_pixel) + ";;"
+                        self.broadcast(message_pixel.encode())
+                        sending_pixel = {"Lock": UID, "loc": (int(x / 5), int(y / 5))}
+                        message_pixel = json.dumps(sending_pixel) + ";;"
+                        self.broadcast(message_pixel.encode())
+                        self.map[x][y] = UID
+                        self.lock_list[int(x / 5)][int(y / 5)] = UID
+                        if self.cell_check(UID, (x, y)):
+                            self.cell_fill_out(UID, (x, y))
+                            i = int(x / 5) * 5
+                            j = int(y / 5) * 5
+                            sending_pixel = {"UID_cell": UID, "loc": (i, j)}
+                            message_pixel = json.dumps(sending_pixel) + ";;"
+                            for i in range(5):
+                                self.broadcast(message_pixel.encode())
+                                time.sleep(0.01)
+
+                else:
+                    UID_lock = np.where(self.lock_list == UID)
+                    lock_row = list(UID_lock[0])
+                    lock_col = list(UID_lock[1])
+                    for i in range(len(lock_row)):
+                        if not self.cell_check(UID,  (int(lock_row[i]*5),int(lock_col[i]*5))):
+                            self.lock_list[lock_row[i]][lock_col[i]] = 0
+                            self.cell_fill_out(0, (lock_row[i] * 5, lock_col[i] * 5))
+                            sending_pixel = {"Lock": 0, "loc": (int(lock_row[i]), int(lock_col[i]))}
+                            message_pixel = json.dumps(sending_pixel) + ";;"
+                            for k in range(2):
+                                self.broadcast(message_pixel.encode())
+                                time.sleep(0.01)
+                            clean_cell = {"UID_cell": 0, "loc": (int(lock_row[i] * 5), int(lock_col[i] * 5))}
+                            clean_message = json.dumps(clean_cell) + ";;"
+                            for i in range(2):
+                                self.broadcast(clean_message.encode())
+                                time.sleep(0.01)
+
+
+        except Exception as e:
+            print("Map running error: details", repr(e))
+            traceback.print_exc()
+
+    def cell_check(self, UID, position):
+        p = position
+        x = int(p[0] / 5) * 5
+        y = int(p[1] / 5) * 5
+        cell = self.map[x:x + 5, y:y + 5]
+        temp = np.sum(cell == UID)
+        if temp / 25 >= 0.5:
+            return True
+        else:
+            return False
+
+    def inGame(self):
+        th3 = threading.Thread(target=self.pixel_proccess)
+        th3.start()
+        # th4 = threading.Thread(target=self.broadcast_map)
+        # th4.start()
+        # self.pixel_proccess()
+
+    def cell_fill_out(self, UID, position):
+        p = position
+        x = int(p[0] / 5) * 5
+        y = int(p[1] / 5) * 5
+        for i in range(5):
+            for j in range(5):
+                self.map[x + i][y + j] = UID
+
+    def run(self):
+        print('Server is starting ...')
+        self.negotiateUID()
+        # time.sleep(1)
+        print('Game in running ...')
+        self.inGame()
+
+
+if __name__ == "__main__":
+    game_server = DrawGameServer(59000, 1)
+    game_server.run()
+    # run()
